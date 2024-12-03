c03999792e6a1846a03100c7359b21f67407d37c
xb <2098780588@qq.com>
2024-12-03 16:12:37 +0800

    feature: 初始化短链接分组功能

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/GroupService.java
@@ -0,0 +1,12 @@
+package com.nageoffer.shortlink.admin.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.nageoffer.shortlink.admin.dao.entity.GroupDO;
+
+/**
+ * @FileName GroupService
+ * @Description 短链接分组接口
+ */
+
+public interface GroupService extends IService<GroupDO> {
+}
