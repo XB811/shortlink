f4ab07a36aa165e1ce5463e4862c2a1b590de8e5
xb <2098780588@qq.com>
2024-12-04 18:28:35 +0800

    feature: 开发短链接后管系统-完善用户信息传输过滤器

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -1,38 +1,35 @@
 package com.nageoffer.shortlink.admin.common.biz.user;
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSON;
 import com.google.common.collect.Lists;
-import com.nageoffer.shortlink.admin.dao.entity.UserDO;
+import com.nageoffer.shortlink.admin.common.constant.UserConstant;
+import com.nageoffer.shortlink.admin.common.convention.exception.ClientException;
 import groovy.util.logging.Slf4j;
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.util.StringUtils;
-import com.nageoffer.shortlink.admin.common.constant.UserConstant;
+
 import java.io.IOException;
-import java.net.URLDecoder;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
-import static java.nio.charset.StandardCharsets.UTF_8;
+import static com.nageoffer.shortlink.admin.common.enums.UserErrorCodeEnum.USER_TOKEN_FAIL;
 
 /**
  * 用户信息传输过滤器
  *
  * @公众号：马丁玩编程，回复：加群，添加马哥微信（备注：12306）获取项目资料
  */
-@lombok.extern.slf4j.Slf4j
 @RequiredArgsConstructor
 @Slf4j
 public class UserTransmitFilter implements Filter {
     private final StringRedisTemplate redisTemplate;
 
-    private static final List<String> IGNORE_URL = Lists.newArrayList(
+    //忽略的url
+    private static final List<String> IGNORE_URI = Lists.newArrayList(
             "/api/short-link/admin/v1/user/login",
             "/api/short-link/admin/v1/user/has-username"
     );
@@ -40,25 +37,39 @@ public class UserTransmitFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        String requestURL = String.valueOf(httpServletRequest.getRequestURL());
-        if(!IGNORE_URL.contains(requestURL)){
-            String token = httpServletRequest.getHeader(UserConstant.USER_TOKEN_KEY);
-            String username = httpServletRequest.getHeader(UserConstant.USER_NAME_KEY);
-            //查询redis
-            Object userInfoJsonStr = redisTemplate.opsForHash().get("login_" + username, token);
-            if (userInfoJsonStr != null) {
+        String requestURI = String.valueOf(httpServletRequest.getRequestURI());
+        //忽略指定的uri
+        if(!IGNORE_URI.contains(requestURI)){
+            String method = httpServletRequest.getMethod();
+            //忽略掉"/api/short-link/admin/v1/user"且请求方法为POST的请求
+            if(!(Objects.equals(requestURI,"/api/short-link/admin/v1/user")&&Objects.equals(method,"POST"))){
+                String token = httpServletRequest.getHeader(UserConstant.USER_TOKEN_KEY);
+                String username = httpServletRequest.getHeader(UserConstant.USER_NAME_KEY);
+                //如果请求中缺少username或者token
+                if(!StrUtil.isAllNotEmpty(username,token)){
+                    throw new ClientException(USER_TOKEN_FAIL);
+                }
+                //查询redis
+                Object userInfoJsonStr =null;
+                try {
+                    userInfoJsonStr = redisTemplate.opsForHash().get("login_" + username, token);
+                    if(userInfoJsonStr ==null){
+                        throw new ClientException(USER_TOKEN_FAIL);
+                    }
+                } catch (Exception e) {
+                    throw new ClientException(USER_TOKEN_FAIL);
+                }
+                //写入ThreadLocal
                 UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
-                //log.info("当前用户："+userInfoDTO.toString());
                 UserContext.setUser(userInfoDTO);
                 //给redis中缓存的当前用户信息续期
                 redisTemplate.expire("login_"+username,30, TimeUnit.MINUTES);
             }
-            try {
-                filterChain.doFilter(servletRequest, servletResponse);
-            } finally {
-                UserContext.removeUser();
-            }
         }
-
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            UserContext.removeUser();
+        }
     }
 }
--- admin/src/main/java/com/nageoffer/shortlink/admin/common/enums/UserErrorCodeEnum.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/enums/UserErrorCodeEnum.java
@@ -8,6 +8,8 @@ import com.nageoffer.shortlink.admin.common.convention.errorcode.IErrorCode;
  * @Description 定义User业务独有的异常码
  */
 public enum UserErrorCodeEnum implements IErrorCode {
+    USER_TOKEN_FAIL("A0002000","用户token校验失败"),
+
     USER_NULL("B000200", "用户记录不存在"),
 
     USER_NAME_EXIST("B000201", "用户名已存在"),
