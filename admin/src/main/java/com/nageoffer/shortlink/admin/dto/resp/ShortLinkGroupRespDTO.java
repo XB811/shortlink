68bb6d21f2635a8e382a8d1eda5ca10bd2d80b42
xb <2098780588@qq.com>
2024-12-04 21:55:05 +0800

    feature: 优化查询短链接分组返回实体

--- admin/src/main/java/com/nageoffer/shortlink/admin/dto/resp/ShortLinkGroupRespDTO.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dto/resp/ShortLinkGroupRespDTO.java
@@ -1,6 +1,5 @@
 package com.nageoffer.shortlink.admin.dto.resp;
 
-import lombok.Builder;
 import lombok.Data;
 
 /**
@@ -18,11 +17,6 @@ public class ShortLinkGroupRespDTO {
      * 分组名称
      */
     private String name;
-
-    /**
-     * 创建分组用户名
-     */
-    private String username;
 
     /**
      * 分组排序
